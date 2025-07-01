eb22974a5c2248b90064da1230278bfb55a1797f
alexnicita <alexandernicita@gmail.com>
2025-07-01 21:55:15 +0000

    Deploying to gh-pages from @ alexnicita/shadcnicita@b0b3e54a80ddbcd1d09c6c31f4d76f15469f05ba 🚀

---
+++ assets/index-ChrcpRjF.js
@@ -0,0 +1,30 @@
+const n=`---
+title: "Hello World"
+date: "2024-12-19"
+description: "Description"
+---
+
+Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Φιλοσοφία τῆς τέχνης καὶ τῶν πραγμάτων ἐστίν.
+
+![The World](/world.svg)
+
+## Ξενοφάνης Κολοφώνιος ⚡ Veritas
+
+Ut enim ad minim veniam, quis nostrud exercitation ullamco **laboris nisi** ut aliquip ex ea commodo consequat. Αἰσχύλος ∞ γράφει τραγῳδίας μεγάλας.
+
+### Duis Aute Irure ᚱᚢᚾᚨ ☽
+
+- Lorem ipsum ⟨𝔞𝔫𝔞𝔯𝔦⟩ consectetur adipiscing elit
+- Vestibulum ante ἱστορία scripta manet ☥
+- Excepteur sint 𓂀 occaecat cupidatat non proident
+- In culpa qui ᚦᚩᚱ officia deserunt mollit ⊕
+
+## Σοφία ∴ Sapientia
+
+Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium. Δημόκριτος λέγει· "πάντα ῥεῖ" ∧ nihil permanet sub sole.
+
+> "Veritas numquam perit ☯ τὸ ἀληθὲς οὐδέποτε ἀπόλλυται" - Αἰσχύλος ⟨MCDXII⟩
+
+Finibus bonorum et malorum... ᚦᚨᚱ ᚱᚢᚾᚨᚱ ᚾᚢ ☾ veniam nostrud!
+`;export{n as default};
+//# sourceMappingURL=index-ChrcpRjF.js.map
