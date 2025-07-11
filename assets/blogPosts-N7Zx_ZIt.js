ce76ac1705b703b715c65061c77aecf279218051
alexnicita <alexandernicita@gmail.com>
2025-07-11 17:54:25 +0000

    Deploying to gh-pages from @ alexnicita/shadcnicita@a6a79b67f27b7c66193f6fb482b98f4e6ece2f84 🚀

---
+++ assets/blogPosts-N7Zx_ZIt.js
@@ -0,0 +1,2 @@
+const t=[{slug:"hello-world",title:"Hello World",date:new Date().toISOString().split("T")[0],description:"Description"},{slug:"sample-post",title:"Sample Post",date:new Date().toISOString().split("T")[0],description:"Another test post for the blog"}];export{t as b};
+//# sourceMappingURL=blogPosts-N7Zx_ZIt.js.map
